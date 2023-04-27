673f34b9db3f27780cacce01fc1b616856fbd8d7
Simone Carlo Surace <simone.surace@artificialy.com>
2023-04-27 16:09:25 +0200

    Fix test failure

--- src/EnzymePrecompilationTest.jl
+++ src/EnzymePrecompilationTest.jl
@@ -5,6 +5,6 @@ using Enzyme
 export myfunction, gradient
 
 myfunction(θ) = θ.α^2
-gradient(f, θ) = only(autodiff(Reverse, f, Active(θ)))
+gradient(f, θ) = only(only(autodiff(Reverse, f, Active(θ))))
 
 end # module EnzymePrecompilationTest
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
 using EnzymePrecompilationTest, Test
 
 θ = (α = 1.0,)
-@test gradient(myfunction, θ) == (α = 2.0)
+@test gradient(myfunction, θ) == (α = 2.0,)
